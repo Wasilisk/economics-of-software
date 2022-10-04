3379702d1dfa3ed8371d4d799d6111809fc487db
Vasyl Petryna <vasyl.petryna@edvantis.com>
2022-10-04 20:41:01 +0300

    feat: add navigation

---
+++ src/features/navigation/ui/index.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+import { Tab } from "shared/ui/tab";
+import { Tabs } from "shared/ui/tabs";
+
+export const Navigation = () => {
+    const [value, setValue] = useState(0);
+
+    const handleChange = (event: React.SyntheticEvent<Element, Event>, newValue: number) => {
+        setValue(newValue);
+    };
+
+    return (
+        <Tabs
+            variant="fullWidth"
+            value={value}
+            onChange={handleChange}
+        >
+            <Tab component={Link} label="Home" to="/" />
+            <Tab component={Link} label="COCOMO" to="/cocomo-calculator" />
+        </Tabs>
+    )
+}
