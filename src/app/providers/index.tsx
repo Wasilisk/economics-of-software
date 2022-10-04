8ddfe9727f05428490640dbd46f908d026b84d6a
Vasyl Petryna <vasyl.petryna@edvantis.com>
2022-10-04 20:35:44 +0300

    feat: add router and theme providers

---
+++ src/app/providers/index.tsx
@@ -0,0 +1,6 @@
+import compose from "compose-function";
+
+import { withRouter } from "./with-router";
+import { withTheme } from "./with-theme";
+
+export const withProviders = compose(withRouter, withTheme);
---
+++ src/app/providers/with-router.tsx
@@ -0,0 +1,8 @@
+import { ReactNode } from "react";
+import { BrowserRouter } from "react-router-dom";
+
+export const withRouter = (component: () => ReactNode) => () => (
+    <BrowserRouter>
+        {component()}
+    </BrowserRouter>
+);
