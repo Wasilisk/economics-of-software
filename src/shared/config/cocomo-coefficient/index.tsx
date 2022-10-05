f258e49492a2acdefa57b41a9256ee231711aa35
Vasyl Petryna <vasyl.petryna@edvantis.com>
2022-10-05 18:21:58 +0300

    feat: add coefficient data

---
+++ src/shared/config/cocomo-coefficient/index.tsx
@@ -0,0 +1,22 @@
+import { CocomoCoefficients } from "./types";
+
+export const cocomoCoefficients: CocomoCoefficients = {
+    organic: {
+        a: 2.4,
+        b: 1.05,
+        c: 2.5,
+        d: 0.38
+    },
+    semidetach: {
+        a: 3,
+        b: 1.12,
+        c: 2.5,
+        d: 0.35
+    },
+    embedded: {
+        a: 3.6,
+        b: 1.2,
+        c: 2.5,
+        d: 0.32
+    }
+}
---
+++ src/shared/config/cocomo-coefficient/types.ts
@@ -0,0 +1,10 @@
+export type CocomoCoefficientType = {
+    a: number,
+    b: number,
+    c: number,
+    d: number
+}
+
+export type CocomoCoefficients = {
+    [key: string]: CocomoCoefficientType
+}
