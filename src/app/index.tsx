31dc5d2e8905fb99d9128ed8e3fa0413f598c2e5
Vasyl Petryna <vasyl.petryna@edvantis.com>
2022-10-05 18:17:52 +0300

    feat: add global styles

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,14 +3,18 @@ import { Layout } from "shared/ui/layout";
 import { Header } from "widgets/header/ui";
 
 import { withProviders } from "./providers";
+import AppStyles from "./styles";
 
 const App = () => (
-  <Layout>
-    <Header />
-    <Layout.Content>
-      <Routing />
-    </Layout.Content>
-  </Layout>
+  <>
+    <Layout>
+      <Header />
+      <Layout.Content>
+        <Routing />
+      </Layout.Content>
+    </Layout>
+    <AppStyles/>
+  </>
 )
 
 export default withProviders(App);
