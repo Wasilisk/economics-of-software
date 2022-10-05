ceb1aa815485fa1d14665fa5731a2aa770848705
Vasyl Petryna <vasyl.petryna@edvantis.com>
2022-10-05 18:29:03 +0300

    feat: add select cost drivers component

---
+++ src/features/select-cost-drivers/ui/index.tsx
@@ -0,0 +1,43 @@
+import { useContext } from "react"
+
+import { CostDriveType } from "shared/config/cost-drivers/types"
+import { CocomoContext } from "shared/libs/context/cocomo"
+import { Box } from "shared/ui/box";
+import { FormControlLabel } from "shared/ui/form-control-label";
+import { Radio } from "shared/ui/radio";
+import { RadioGroup } from "shared/ui/radio-group";
+import { Typography } from "shared/ui/typography";
+
+export const SelectCostDrivers = ({ id, name, values }: CostDriveType) => {
+    const {costDrivers, setCostDrivers} = useContext(CocomoContext);
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setCostDrivers({...costDrivers, [id]: parseFloat(event.target.value)});
+    };
+
+    return (
+        <Box display="flex" alignItems="center" width="100%">
+            <Box width="130px" mr={1}>
+                <Typography textAlign="center">{name}</Typography>
+            </Box>
+            <Box display="flex" width="100%">
+                <RadioGroup
+                    row
+                    defaultValue={1}
+                    onChange={handleChange}
+                    sx={{flex: 1, justifyContent: "space-around"}}
+                >
+                    {
+                        Object.keys(values).map(key=> <FormControlLabel
+                            labelPlacement="bottom"
+                            value={values[key]}
+                            control={<Radio />}
+                            disabled={!Boolean(values[key])}
+                            label={values[key] ? values[key] : "-"}
+                        />)
+                    }
+                </RadioGroup>
+            </Box>
+        </Box>
+    )
+}
